b9fbc7108394268a4118ee4469f65c53b34f3e35
Facundo Araujo <facuaaraujo@gmail.com>
2020-05-23 18:20:46 -0300

    Updates

---
+++ precache-manifest.89f8ec21742fcb91d1c91ac74e916f73.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5dc7e068fc3daad4cda43d00ee0567f",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "382be10f6faf40cd457f",
+    "url": "/weather-app-react/static/css/main.9c284353.chunk.css"
+  },
+  {
+    "revision": "4878a3ce39281cb262a0",
+    "url": "/weather-app-react/static/js/2.5fbc419f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.5fbc419f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382be10f6faf40cd457f",
+    "url": "/weather-app-react/static/js/main.700ee23e.chunk.js"
+  },
+  {
+    "revision": "103c236b0052d72123ea",
+    "url": "/weather-app-react/static/js/runtime-main.5ba76ca4.js"
+  },
+  {
+    "revision": "b3996514b7a64e164b0e87145d4c4ddf",
+    "url": "/weather-app-react/static/media/fila1-lluvia.b3996514.svg"
+  },
+  {
+    "revision": "4273246a19aa443f855e5782481f32ac",
+    "url": "/weather-app-react/static/media/fila1-niebla.4273246a.svg"
+  },
+  {
+    "revision": "1fa749fbce0231c93d9d3b5b681e5c79",
+    "url": "/weather-app-react/static/media/fila1-nieve.1fa749fb.svg"
+  },
+  {
+    "revision": "84ad2b375bb4faca515624d7db85e555",
+    "url": "/weather-app-react/static/media/fila2-lluvia.84ad2b37.svg"
+  },
+  {
+    "revision": "0cd5ab1cd06b0d9cbc32d9c4d50e0839",
+    "url": "/weather-app-react/static/media/fila2-niebla.0cd5ab1c.svg"
+  },
+  {
+    "revision": "8c116cb37b30c0224254f81938bf3ed1",
+    "url": "/weather-app-react/static/media/fila2-nieve.8c116cb3.svg"
+  },
+  {
+    "revision": "3f67f66265fc78b17e96371f7dd2fcee",
+    "url": "/weather-app-react/static/media/fila3-lluvia.3f67f662.svg"
+  },
+  {
+    "revision": "e360185eccf0e2b437820d4f90842b58",
+    "url": "/weather-app-react/static/media/fila3-nieve.e360185e.svg"
+  },
+  {
+    "revision": "3231c0e94a9191c5ede193e76be09c2d",
+    "url": "/weather-app-react/static/media/fila4-lluvia.3231c0e9.svg"
+  },
+  {
+    "revision": "e9a1d164dcce0cacaf09cbd33b639cf2",
+    "url": "/weather-app-react/static/media/fila4-nieve.e9a1d164.svg"
+  },
+  {
+    "revision": "ed51290ae492b764340bebd6e2fc11fe",
+    "url": "/weather-app-react/static/media/fila5-lluvia.ed51290a.svg"
+  },
+  {
+    "revision": "2dd372b9c7b4383baefcb60128daed08",
+    "url": "/weather-app-react/static/media/fila5-nieve.2dd372b9.svg"
+  },
+  {
+    "revision": "58b75ca6f98bc0b35babbb40150a9742",
+    "url": "/weather-app-react/static/media/luna.58b75ca6.svg"
+  },
+  {
+    "revision": "2dc64c7747a4906faf06d0be8938189c",
+    "url": "/weather-app-react/static/media/nube-clara.2dc64c77.svg"
+  },
+  {
+    "revision": "87af4bde9c0dd19b2b3f8c8a05e72820",
+    "url": "/weather-app-react/static/media/nube-mas-oscura.87af4bde.svg"
+  },
+  {
+    "revision": "3840d36e8ae4ee0f459448c698f0ed40",
+    "url": "/weather-app-react/static/media/nube-negra.3840d36e.svg"
+  },
+  {
+    "revision": "e9b158caeb6e04b3c2b8857ec839037f",
+    "url": "/weather-app-react/static/media/nube-simple.e9b158ca.svg"
+  },
+  {
+    "revision": "a2d49ec771ff169f6d9e24aa22baec6c",
+    "url": "/weather-app-react/static/media/rayo.a2d49ec7.svg"
+  },
+  {
+    "revision": "466f80268d1b156e500f2440f54cd5ea",
+    "url": "/weather-app-react/static/media/sol.466f8026.svg"
+  }
+]);
